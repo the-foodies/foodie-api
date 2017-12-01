5621478cd82431945cea700a2c4a04e75f38cb46
Zac St Louis <p2kmafia@gmail.com>
2017-11-30 19:18:52 -0800

    added cors header to rest server

--- rest-server/src/index.ts
+++ rest-server/src/index.ts
@@ -14,6 +14,12 @@ import { router } from './routes';
 const app = express();
 const client = redis.createClient();
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:1337');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(session({
   secret: 'get dat paper yo',
   saveUninitialized: false,
