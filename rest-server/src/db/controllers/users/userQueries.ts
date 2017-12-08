3d0496d25ce77d8ef1331ab672dc6d05f2eb512d
Max Lavicka <max.lavicka@gmail.com>
2017-12-08 11:27:47 -0800

    Added get posts route

--- rest-server/src/db/controllers/users/userQueries.ts
+++ rest-server/src/db/controllers/users/userQueries.ts
@@ -18,8 +18,48 @@ const getUserById = async (id) => {
   });
   return user;
 };
+const getUserPosts = async (userId) => {
+  const recipes = await db.Users.findOne({
+    where: {
+      id: userId,
+    },
+    include: [{
+      model: db.Recipes,
+      include: [
+        { model: db.Ingredients },
+        { model: db.Directions },
+        { model: db.ImagesRecipes },
+        { model: db.Tags },
+        { model: db.Comments },
+      ]
+    }]
+  });
+  const restaurants = await db.Users.findOne({
+    where: {
+      id: userId,
+    },
+    include: [{
+      model: db.Restaurants,
+      include: [
+        { model: db.FoodItems },
+        { model: db.ImagesRestaurants},
+        { model: db.Tags },
+        { model: db.Comments},
+      ]
+    }]
+  })
+  let posts = [];
+  await recipes.Recipes.forEach(recipe => {
+    posts.push(recipe);
+  });
+  await restaurants.Restaurants.forEach(restaurant => {
+    posts.push(restaurant);
+  });
+  return posts;
+}
 
 export {
   getUserByEmail,
   getUserById,
+  getUserPosts,
 };
