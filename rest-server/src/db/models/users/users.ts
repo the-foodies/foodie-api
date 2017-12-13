459f7b3415bd026d06c15d3e1b33610956b94376
Max Lavicka <max.lavicka@gmail.com>
2017-12-13 12:27:42 -0800

    Fixed merge conflicts

--- rest-server/src/db/models/users/users.ts
+++ rest-server/src/db/models/users/users.ts
@@ -19,16 +19,13 @@ module.exports = (Sequelize, DataTypes) => {
     Users.belongsToMany(models.Restaurants, {
       through: 'restaurants_users',
     });
-<<<<<<< 89510c62e9117d76717884d8f41418afe5c4f6d0
     Users.hasMany(models.Comments, {
       as: 'poster',
     });
     Users.hasMany(models.Comments);
     Users.hasMany(models.FoodItems);
     Users.hasMany(models.ImagesRestaurants);
-=======
     Users.hasMany(models.Subscriptions);
->>>>>>> Server controllers re-formatted and subscription controllers completed
   };
 
   return Users;
