0e83b6846958d94a7ac65b8593dea1d2be5feacf
Max Lavicka <max.lavicka@gmail.com>
2017-12-15 11:04:50 -0800

    Unsubscribe route/db controller added

--- rest-server/src/db/controllers/subscriptions/index.ts
+++ rest-server/src/db/controllers/subscriptions/index.ts
@@ -1,8 +1,10 @@
 import createSubscription from './createSubscription';
+import removeSubscription from './removeSubscription';
 import { getSubscriptionsById, isUserSubscribed } from './subscriptionQueries';
 
 export {
   createSubscription,
+  removeSubscription,
   getSubscriptionsById,
   isUserSubscribed,
 };
